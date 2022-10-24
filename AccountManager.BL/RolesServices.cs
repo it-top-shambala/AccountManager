60bd947e9501030dd41354946779514db14cdb69
Starinin Andrey <starininandrey@gmail.com>
2022-10-24 18:23:24 +0300

    Add BL proj

---
+++ AccountManager.BL/RolesServices.cs
@@ -0,0 +1,39 @@
+﻿using AccountManager.DAL;
+using AccountManager.Models;
+
+namespace AccountManager.BL;
+
+public class RolesServices
+{
+    private readonly ICrud<Role> _crud;
+
+    public RolesServices(ICrud<Role> crud)
+    {
+        _crud = crud;
+    }
+
+    public void Insert(Role role)
+    {
+        _crud.Insert(role);
+    }
+
+    public IEnumerable<Role> GetAll()
+    {
+        return _crud.GetAll();
+    }
+
+    public Role GetById(int id)
+    {
+        return _crud.GetById(id);
+    }
+
+    public void Update(Role role)
+    {
+        _crud.Update(role);
+    }
+
+    public void Delete(int id)
+    {
+        _crud.Delete(id);
+    }
+}
