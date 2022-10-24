c6e834e5254c355bc57b6eecad34fa0a69aded1d
Starinin Andrey <starininandrey@gmail.com>
2022-10-24 16:11:58 +0300

    Add models proj

---
+++ AccountManager.Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountManager.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public bool IsDelete { get; set; }
+    public int RoleId { get; set; }
+}
